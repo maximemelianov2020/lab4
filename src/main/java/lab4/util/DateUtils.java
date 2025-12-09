c446e169bf619acfb624dcaeb67e869ab055fc5d
mateb <maximemelianov2020@gmail.com>
2025-12-10 00:46:43 +0300

    Initial commit

---
+++ src/main/java/lab4/util/DateUtils.java
@@ -0,0 +1,4 @@
+package lab4.util;
+
+public class DateUtils {
+}
---
+++ src/main/java/org/example/lab4/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.lab4;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}

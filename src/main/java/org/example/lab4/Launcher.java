a15f6a3916c37b855938fae9c6a38dba8b4c97ae
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-10 08:56:08 +0300

    Delete src/main/java/org/example/lab4 directory

--- src/main/java/org/example/lab4/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.lab4;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}

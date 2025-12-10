edeea8097021c1def211c99dfee3208e3071878b
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-10 08:58:24 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module org.example.lab4 {
+    requires com.opencsv;
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens lab4 to javafx.fxml;
+    opens lab4.model to javafx.fxml;
+    opens lab4.csv to javafx.fxml;
+    opens lab4.util to javafx.fxml;
 
-    opens org.example.lab4 to javafx.fxml;
-    exports org.example.lab4;
+    exports lab4;
+    exports lab4.model;
+    exports lab4.csv;
+    exports lab4.util;
 }
